dd108f83bb1b788e0c91f62814a0f940a8ff1c50
Jin Chen <chenjin10@gmail.com>
2016-08-10 19:07:10 +1200

    Implemented methods in AgeRangerController.

--- AgeRanger/AgeRanger/Controllers/AgeRangerController.cs
+++ AgeRanger/AgeRanger/Controllers/AgeRangerController.cs
@@ -2,6 +2,8 @@
 using System.Net;
 using System.Web.Http;
 using AgeRanger.Models;
+using AgeRanger.Services;
+using AgeRanger.Utilities;
 using System.Collections.Generic;
 
 namespace AgeRanger.Controllers
@@ -9,54 +11,77 @@ namespace AgeRanger.Controllers
     [RoutePrefix("people")]
     public class AgeRangerController : ApiController
     {
+        private IAgeRangerService _ageRangerService = new AgeRangerService();
 
         [Route]
         [HttpGet]
         public List<Person> GetAll()
         {
-            throw new NotImplementedException();
+            return _ageRangerService.GetAllPeople();
         }
 
         [Route]
         [HttpGet]
         public List<Person> SearchByFirstName(string firstName)
         {
-            throw new NotImplementedException();
+            return _ageRangerService.SearchPersonByFirstName(firstName);
         }
 
         [Route]
         [HttpGet]
         public List<Person> SearchByLastName(string lastName)
         {
-            throw new NotImplementedException();
+            return _ageRangerService.SearchPersonByLastName(lastName);
         }
 
         [Route("{id}")]
         [HttpGet]
         public Person GetPerson(int id)
         {
-            throw new NotImplementedException();
+            return _ageRangerService.GetPersonById(id);
         }
 
         [Route]
         [HttpPost]
         public void Create(Person person)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _ageRangerService.AddPerson(person);
+            }catch (Exception)
+            {
+                var response = HttpResponseFactory.ConstructResponse(HttpStatusCode.BadRequest, "Create new person failed, please check your input.");
+                throw new HttpResponseException(response);
+            }
+
         }
 
         [Route("{id}")]
         [HttpPut]
         public void Update(int id, Person person)
         {
-            throw new NotImplementedException();
+            if (person == null)
+            {
+                string errMessage = "Person cannot be null.";
+                var response = HttpResponseFactory.ConstructResponse(HttpStatusCode.BadRequest, errMessage);
+                throw new HttpResponseException(response);
+            }
+            _ageRangerService.UpdatePerson(id, person);
         }
 
         [Route("{id}")]
         [HttpDelete]
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _ageRangerService.DeletePerson(id);
+            } catch (Exception)
+            {
+                var response = HttpResponseFactory.ConstructResponse(HttpStatusCode.BadRequest, "Delete failed, please try again.");
+                throw new HttpResponseException(response);
+            }
+
         }
     }
 }
---
+++ AgeRanger/AgeRanger/Dao/Helpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web;
+
+namespace AgeRanger.Dao
+{
+    public class Helpers
+    {
+        private const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={DataDirectory}\Database.mdf;Integrated Security=True";
+
+        public static SqlConnection NewConnection()
+        {
+            var connstr = "";
+            if (HttpContext.Current != null)
+            {
+                connstr = ConnectionString.Replace("{DataDirectory}", HttpContext.Current.Server.MapPath("~/App_Data"));
+            }
+            else
+            {
+                var separator = Path.DirectorySeparatorChar;
+                var currentPath = AppDomain.CurrentDomain.BaseDirectory;
+                var dataPath = currentPath.Substring(0, currentPath.LastIndexOf(separator + "refactor-me.Tests"));
+                connstr = ConnectionString.Replace("{DataDirectory}", dataPath + separator + "refactor-me" + separator + "App_Data");
+            }
+            return new SqlConnection(connstr);
+        }
+
+    }
+}
